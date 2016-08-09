29921745bed4f1d280f1f56d3c76a8c70c8c5341
cooltoast <mpatel678@gmail.com>
2016-08-08 18:55:49 -0700

    update tests

--- static/js/test.js
+++ static/js/test.js
@@ -1,14 +1,12 @@
 var assert = require('assert');
 var VNode = require('./VirtualNode');
 var VirtualDiff = require('./VirtualDiff');
-var ApplyPatches = require('./ApplyPatches');
 var Patch = require('./Patch');
 var _ = require('lodash');
 
-var numberTree = VirtualDiff['numberTree'];
 var diff = VirtualDiff['diff'];
-var applyPatches = ApplyPatches['applyPatches'];
-var buildDOMTree = ApplyPatches['buildDOMTree'];
+var applyPatches = VirtualDiff['applyPatches'];
+var buildDOMTree = VirtualDiff['buildDOMTree'];
 
 var a,b,c,d,e,f;
 
@@ -22,48 +20,9 @@ describe('VirtualNode', function() {
 });
 
 describe('VirtualDiff', function() {
-  describe('#numberTree()', function() {
-    it('should index simple trees', function() {
-      a = new VNode(1, null, '');
-      b = new VNode(1, null, '', 0);
-      numberTree(a);
-      assert(_.isEqual(a,b));
-    });
-
-    it('should index small trees', function() {
-      a = new VNode(1, null, '');
-      a.appendChild(new VNode(2,null, ''));
-      a.appendChild(new VNode(3,null, ''));
-      b = new VNode(1, null, '', 0);
-      b.appendChild(new VNode(2,null, '',1));
-      b.appendChild(new VNode(3,null, '',2));
-      numberTree(a);
-      assert(_.isEqual(a,b));
-    });
-
-    it('should index medium trees', function() {
-      a = new VNode(1, null, '');
-      var aChild = new VNode(2,null, '');
-      aChild.appendChild(new VNode(3,null, ''));
-      aChild.appendChild(new VNode(4,null, ''));
-      a.appendChild(aChild);
-      a.appendChild(new VNode(5,null, ''));
-      a.appendChild(new VNode(6,null, ''));
-
-      b = new VNode(1, null, '',0);
-      var bChild = new VNode(2,null, '',1);
-      bChild.appendChild(new VNode(3,null, '',2));
-      bChild.appendChild(new VNode(4,null, '',3));
-      b.appendChild(bChild);
-      b.appendChild(new VNode(5,null, '',4));
-      b.appendChild(new VNode(6,null, '',5));
-
-      numberTree(a);
-      assert(_.isEqual(a,b));
-    });
-  });
-
   describe('#diff()', function() {
+    var ans;
+
     it('should not diff same nodeName and value', function() {
       a = new VNode(1, null,'div');
       b = new VNode(1, null,'div');
@@ -73,31 +32,41 @@ describe('VirtualDiff', function() {
     it('should node_replace same nodename, diff value', function() {
       a = new VNode(1, null,'div');
       b = new VNode(2, null,'div');
-      assert(_.isEqual(diff(a,b), {0:[new Patch('node_replace', b)]}));
+      ans = {};
+      ans[a.id] = [new Patch('node_replace', b)];
+      assert(_.isEqual(diff(a,b), ans));
     });
 
     it('should node_replace diff nodename, both simpleNodename, and same value', function() {
       a = new VNode(1, null,'div');
       b = new VNode(1, null,'p');
-      assert(_.isEqual(diff(a,b), {0:[new Patch('node_replace', b)]}));
+      ans = {};
+      ans[a.id] = [new Patch('node_replace', b)];
+      assert(_.isEqual(diff(a,b), ans));
     });
 
     it('should tree_replace diff nodename, not both simpleNodename, and same value', function() {
       a = new VNode(1, null,'div');
       b = new VNode(1, null,'table');
-      assert(_.isEqual(diff(a,b), {0:[new Patch('tree_replace', b)]}));
+      ans = {};
+      ans[a.id] = [new Patch('tree_replace', b)];
+      assert(_.isEqual(diff(a,b), ans));
     });
 
     it('should node_replace diff nodename, both simpleNodename, and diff value', function() {
       a = new VNode(1, null,'div');
       b = new VNode(2, null,'p');
-      assert(_.isEqual(diff(a,b), {0:[new Patch('node_replace', b)]}));
+      ans = {};
+      ans[a.id] = [new Patch('node_replace', b)];
+      assert(_.isEqual(diff(a,b), ans));
     });
 
     it('should tree_replace diff nodename, not both simpleNodename, and diff value', function() {
       a = new VNode(1, null,'div');
       b = new VNode(2, null,'table');
-      assert(_.isEqual(diff(a,b), {0:[new Patch('tree_replace', b)]}));
+      ans = {};
+      ans[a.id] = [new Patch('tree_replace', b)];
+      assert(_.isEqual(diff(a,b), ans));
     });
 
     it('should find an insert', function() {
@@ -107,7 +76,9 @@ describe('VirtualDiff', function() {
       f = new VNode(5, null);
       a = new VNode(1, [c,d,e]);
       b = new VNode(1, [c,d,e,f]);
-      assert(_.isEqual(diff(a,b), {0:[new Patch('insert', f)]}));
+      ans = {};
+      ans[a.id] = [new Patch('insert', f)];
+      assert(_.isEqual(diff(a,b), ans));
     });
 
     it('should find a node_replace and insertion', function() {
@@ -117,7 +88,9 @@ describe('VirtualDiff', function() {
       f = new VNode(5, null);
       a = new VNode(1, [c,d,e], 'div');
       b = new VNode(10, [c,d,e,f], 'div');
-      assert(_.isEqual(diff(a,b), {0:[new Patch('node_replace', b), new Patch('insert', f)]}));
+      ans = {};
+      ans[a.id] = [new Patch('node_replace', b), new Patch('insert', f)];
+      assert(_.isEqual(diff(a,b), ans));
     });
 
     it('should find a tree_replace and insertion', function() {
@@ -127,7 +100,9 @@ describe('VirtualDiff', function() {
       f = new VNode(5, null);
       a = new VNode(1, [c,d,e], 'div');
       b = new VNode(10, [c,d,e,f], 'table');
-      assert(_.isEqual(diff(a,b), {0:[new Patch('tree_replace', b)]}));
+      ans = {};
+      ans[a.id] = [new Patch('tree_replace', b)];
+      assert(_.isEqual(diff(a,b), ans));
     });
 
     it('should find a deletion', function() {
@@ -137,7 +112,9 @@ describe('VirtualDiff', function() {
       f = new VNode(5, null);
       a = new VNode(1, [c,d,e,f]);
       b = new VNode(1, [c,d,e]);
-      assert(_.isEqual(diff(a,b), {4:[new Patch('delete')]}));
+      ans = {};
+      ans[f.id] = [new Patch('delete')];
+      assert(_.isEqual(diff(a,b), ans));
     });
 
     it('should find multiple insertions', function() {
@@ -147,7 +124,9 @@ describe('VirtualDiff', function() {
       f = new VNode(5, null);
       a = new VNode(1);
       b = new VNode(1, [c,d,e,f]);
-      assert(_.isEqual(diff(a,b), {0:[new Patch('insert', c),new Patch('insert', d),new Patch('insert', e),new Patch('insert', f)]}), "wrong answer");
+      ans = {};
+      ans[a.id] = [new Patch('insert', c),new Patch('insert', d),new Patch('insert', e),new Patch('insert', f)];
+      assert(_.isEqual(diff(a,b), ans));
     });
   });
 });
